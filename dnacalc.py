4e5dba2f2b27d183ee665ede778a9204dcbb8521
Giannis Kesidis <giannis.kesidis@geo.uu.se>
2016-06-03 19:30:37 +0200

    added melt temp

--- dnacalc.py
+++ dnacalc.py
@@ -2,7 +2,7 @@
 
 #the original DNASeq is immediately written over when I give it another meaning
 DNASeq = "ATGAAC"
-DNASeq = raw_input(" Enter a DNA sequence: ")
+#DNASeq = raw_input(" Enter a DNA sequence: ")
 DNASeq = DNASeq.upper()
 DNASeq = DNASeq.replace(" ","")
 
@@ -20,8 +20,28 @@ NumberT = DNASeq.count('T')
 NumberG = DNASeq.count('G')
 NumberC = DNASeq.count('C')
 
-print('A: ' + str ( NumberA / SeqLength ))
-print('T: ' + str ( NumberT / SeqLength ))
-print('G: ' + str ( NumberG / SeqLength ))
-print('C: ' + str ( NumberC / SeqLength ))
 
+print ("A: {}".format (NumberA / SeqLength))
+print ("T: {}".format (NumberA / SeqLength))
+print ("G: {}".format (NumberA / SeqLength))
+print ("C: {}".format (NumberA / SeqLength))
+
+TotalStrong = NumberG + NumberC
+TotalWeak = NumberA + NumberT
+
+if SeqLength <= 14:
+
+	MeltTemp = ( 4 * TotalStrong ) + ( 2 * TotalWeak )
+
+	#print ( 'Melting Temp: {}').format(MeltTemp)
+
+else:
+	MeltTemp = 64.9 + 41 * (TotalStrong - 16.4 / SeqLength)
+
+	#print ( 'Long melting Temp: {}'.format(MeltTemp))
+
+print ( 'Melting Temp: {}'.format(MeltTemp))
+
+print ('Done')
+
+#if is a way to say which lines of codes are executed depending on other parameters)

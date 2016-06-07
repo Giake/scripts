0d69116c9cd6b68cb7b16a2f79b084444d976276
Giannis Kesidis <giannis.kesidis@geo.uu.se>
2016-06-07 11:59:31 +0200

    added DecimalDegree

--- latlon.py
+++ latlon.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import re
+
 InFileName = 'Marrus_claudanielis.txt'
 
 InFile = open ( InFileName, 'r')
@@ -16,5 +18,29 @@ for Line in InFile:
 		#print ( Elementlist )
 		print ( 'Depth:{} Lat:{} Lon:{}'.format (Elementlist[4], Elementlist[2], Elementlist[3]))
 
+
+		SearchString = '(\d+) ([\d\.]+) (\w)'
+		Result = re.search( SearchString, Elementlist[2])
+		print ( Result.group (1))
+		print ( Result.group (2))
+		print (	Result.group (3))
+
+		Degree = float (Result.group( 1 ))
+
+		Minute = float (Result.group( 2 ))
+
+		Compass = Result.group( 3 )
+
+		DecimalDegree = Degree + Minute / 60
+		if Compass in 'SW':
+			DecimalDegree = - DecimalDegree
+			
+		print DecimalDegree
+
+		#print( Degree ) 
+
+		#print( Minute ) 
+
+		#print( Compass ) 
 
 	LineNumber = LineNumber + 1

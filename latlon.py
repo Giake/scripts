5df231810d50ad93752353cd34a79a7aea822ad9
Giannis Kesidis <giannis.kesidis@geo.uu.se>
2016-06-07 11:32:55 +0200

    added split

--- latlon.py
+++ latlon.py
@@ -4,6 +4,17 @@ InFileName = 'Marrus_claudanielis.txt'
 
 InFile = open ( InFileName, 'r')
 
+LineNumber= 0
+
 for Line in InFile:
 	Line = Line.strip()
-	print (Line)
+
+	if LineNumber > 0:
+		#print (LineNumber)
+		#print (Line)
+		Elementlist = Line.split( '\t' )
+		#print ( Elementlist )
+		print ( 'Depth:{} Lat:{} Lon:{}'.format (Elementlist[4], Elementlist[2], Elementlist[3]))
+
+
+	LineNumber = LineNumber + 1
